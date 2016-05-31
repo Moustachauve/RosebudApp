b87a25123414c5de681385dcb62291f35986bd3c
Christophe Gagnier <admin@cgagnier.ca>
2016-05-31 15:44:08 -0400

    index on master: 38c5fa5 Initial commit

---
+++ GtfsAPI/GtfsApi/routes/feed.js
@@ -0,0 +1,13 @@
+﻿var express = require('express')
+var feedAccessor = require('../lib/dataAccessor/feedAccessor.js')
+var router = express.Router()
+
+router.get('/', function (req, res, next) {
+	feedAccessor.getAllFeeds(function (err, data) {
+		if (err) return next(err)
+
+		res.json(data)
+	})
+})
+
+module.exports = router
