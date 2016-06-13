d83b3cb33600928eb545ce241468878c3eec9fd7
Christophe Gagnier <admin@cgagnier.ca>
2016-06-13 18:24:53 -0400

    Add basic search in Feed list

--- MyTransit.Android/Adapters/FeedAdapter.cs
+++ MyTransit.Android/Adapters/FeedAdapter.cs
@@ -4,24 +4,46 @@ using Android.Content;
 using Android.Views;
 using Android.Widget;
 using MyTransit.Core;
+using System.Linq;
+using MyTransit.Android;
+using Android.App;
 
 namespace MyTransit
 {
 	public class FeedAdapter : BaseAdapter<Feed>
 	{
 		LayoutInflater inflater;
-		private List<Feed> feeds;
 
-		public override Feed this[int position] { get { return feeds[position]; } }
-		public override int Count { get { return feeds.Count; } }
+        private Context context;
+        private List<Feed> feeds;
+        private List<Feed> filteredFeeds;
+        private string filter;
 
-		public FeedAdapter(Context context, List<Feed> feeds)
+        public override Feed this[int position] { get { return filteredFeeds[position]; } }
+		public override int Count { get { return filteredFeeds.Count; } }
+
+        public string Filter
+        {
+            get { return filter; }
+            set
+            {
+                if (value.ToLower() == filter)
+                    return;
+
+                filter = value.ToLower();
+                ApplyFilter();
+            }
+        }
+
+        public FeedAdapter(Context context, List<Feed> feeds)
 		{
+            this.context = context;
 			this.inflater = LayoutInflater.FromContext(context);
 			this.feeds = feeds;
+            ApplyFilter();
 		}
 
-		public override long GetItemId(int position) { return position; }
+        public override long GetItemId(int position) { return filteredFeeds[position].feed_id; }
 
 		public override View GetView(int position, View convertView, ViewGroup parent)
 		{
@@ -43,7 +65,18 @@ namespace MyTransit
 		public void ReplaceData(List<Feed> feeds)
 		{
 			this.feeds = feeds;
-			NotifyDataSetChanged();
-		}
-	}
+            ApplyFilter();
+        }
+
+        private void ApplyFilter()
+        {
+            if (string.IsNullOrWhiteSpace(filter))
+                filteredFeeds = new List<Feed>(feeds);
+            else
+                filteredFeeds = feeds.Where(feed => feed.agency_name.ToLower().Contains(filter)).ToList();
+
+            
+            NotifyDataSetChanged();
+        }
+    }
 }
--- MyTransit.Android/MainActivity.cs
+++ MyTransit.Android/MainActivity.cs
@@ -1,55 +1,83 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
 using MyTransit.Core;
 using Android.Views;
 using Android.Content;
 using Newtonsoft.Json;
+using Android.Support.V7.App;
+using Android.Widget;
+using ToolbarCompat = Android.Support.V7.Widget.Toolbar;
+using SearchViewCompat = Android.Support.V7.Widget.SearchView;
+using System;
+using Android.Support.V4.View;
+using Android.Runtime;
 
 namespace MyTransit.Android
 {
-	[Activity(Label = "MyTransit", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
-	{
-		private FeedAdapter feedAdapter;
-		private ListView feedListView;
-		private ProgressBar feedProgressBar;
+    [Activity(Label = "MyTransit", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        private FeedAdapter feedAdapter;
+        private ListView feedListView;
+        private ProgressBar feedProgressBar;
 
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
-			SetContentView(Resource.Layout.Main);
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
 
-			feedListView = FindViewById<ListView>(Resource.Id.feed_listview);
-			feedProgressBar = FindViewById<ProgressBar>(Resource.Id.feed_progress_bar);
+            var toolbar = FindViewById<ToolbarCompat>(Resource.Id.my_awesome_toolbar);
+            SetSupportActionBar(toolbar);
 
-			LoadFeeds();
+            feedListView = FindViewById<ListView>(Resource.Id.feed_listview);
+            feedProgressBar = FindViewById<ProgressBar>(Resource.Id.feed_progress_bar);
 
-			feedListView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args)
-			{
-				Feed clickedFeed = feedAdapter[args.Position];
-				Intent detailsIntent = new Intent(this, typeof(FeedDetailsActivity));
-				detailsIntent.PutExtra("feedInfos", JsonConvert.SerializeObject(clickedFeed));
-				StartActivity(detailsIntent);
-			};
-		}
+            feedListView.TextFilterEnabled = true;
 
-		private async void LoadFeeds()
-		{
-			feedProgressBar.Visibility = ViewStates.Visible;
-			var feeds = await FeedAccessor.GetAllFeeds();
+            LoadFeeds();
 
-			if (feedAdapter == null)
-			{
-				feedAdapter = new FeedAdapter(this, feeds);
-				feedListView.Adapter = new FeedAdapter(this, feeds);
-			}
-			else
-				feedAdapter.ReplaceData(feeds);
-			
-			feedProgressBar.Visibility = ViewStates.Gone;
-		}
-	}
+            feedListView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args)
+                    {
+                        Feed clickedFeed = feedAdapter[args.Position];
+                        Intent detailsIntent = new Intent(this, typeof(FeedDetailsActivity));
+                        detailsIntent.PutExtra("feedInfos", JsonConvert.SerializeObject(clickedFeed));
+
+                        StartActivity(detailsIntent);
+                    };
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+
+            var searchItem = menu.FindItem(Resource.Id.action_search);
+            var test = MenuItemCompat.GetActionView(searchItem);
+            SearchViewCompat searchView = test.JavaCast<SearchViewCompat>();
+
+            searchView.QueryTextSubmit += (sender, args) =>
+            {
+                feedAdapter.Filter = args.Query;
+            };
+
+            return true;
+        }
+
+        private async void LoadFeeds()
+        {
+            feedProgressBar.Visibility = ViewStates.Visible;
+            var feeds = await FeedAccessor.GetAllFeeds();
+
+            if (feedAdapter == null)
+            {
+                feedAdapter = new FeedAdapter(this, feeds);
+                feedListView.Adapter = feedAdapter;
+            }
+            else
+                feedAdapter.ReplaceData(feeds);
+
+            feedProgressBar.Visibility = ViewStates.Gone;
+        }
+    }
 }
 
 
