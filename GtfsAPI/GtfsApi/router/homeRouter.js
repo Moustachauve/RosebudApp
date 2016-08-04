e3f94d0e142121a98be2c60246a903dc1c94667f
Christophe Gagnier <admin@cgagnier.ca>
2016-08-04 02:54:23 -0400

    Moved GtfsApi and GtfsUpdater to their own repository

--- GtfsAPI/GtfsApi/router/homeRouter.js
+++
@@ -1,9 +0,0 @@
-﻿var express = require('express')
-var router = express.Router()
-
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.json({ title: 'Express' })
-})
-
-module.exports = router
--- GtfsAPI/Sql/StoredProc/executeScripts.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat *.sql | mysql -u root
